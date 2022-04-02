b193bdbb560dcf4b3f57a18f8ba23e2eda173d70
HuynhNgocThuong <huynghngocthuong0208@gmail.com>
2022-04-02 12:03:42 +0700

    [Resolve]: isPalindrome problem

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,22 @@
-function isPalindrome(s) {}
+/**
+ * Time complexity: O(N)
+ * Space complexity: O(1) Left and Right pointers take up constant space
+ */
+function isPalindrome(s) {
+  //Sanitize the input string
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
