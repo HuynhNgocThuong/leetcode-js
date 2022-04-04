e6428fa203b04214bdce805a5677254ba5d2db13
HuynhNgocThuong <huynghngocthuong0208@gmail.com>
2022-04-04 10:30:35 +0700

    [Add]: Resolve longest palindrome problems

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,34 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+/**
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
+ */
+function longestPalindrome(s) {
+  let startIndex = 0;
+  let maxLength = 1;
+
+  function expandArroundMiddle(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        maxLength = currentPalLength;
+        startIndex = left;
+        console.log(maxLength);
+        console.log(startIndex);
+        console.log("----------------");
+      }
+      left -= 1;
+      right += 1;
+    }
+  }
+
+  for (let index = 0; index < s.length; index++) {
+    expandArroundMiddle(index - 1, index + 1);
+    expandArroundMiddle(index, index + 1);
+  }
+  return s.slice(startIndex, startIndex + maxLength);
+}
 
 module.exports = longestPalindrome;
